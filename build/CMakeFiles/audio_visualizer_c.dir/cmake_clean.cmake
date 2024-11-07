cc3ca2ddd0a11c8753c1bf95876b9b12a0ca3f88
Fedmichard <francoisfedmichard@gmail.com>
2024-11-06 23:03:26 -0500

    Setup!

---
+++ build/CMakeFiles/audio_visualizer_c.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/audio_visualizer_c.dir/src/main.c.obj"
+  "CMakeFiles/audio_visualizer_c.dir/src/main.c.obj.d"
+  "audio_visualizer_c.exe"
+  "audio_visualizer_c.exe.manifest"
+  "audio_visualizer_c.pdb"
+  "libaudio_visualizer_c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/audio_visualizer_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/audio_visualizer_c.dir/src/main.c.obj.d
@@ -0,0 +1,11 @@
+CMakeFiles/audio_visualizer_c.dir/src/main.c.obj: \
+ C:\Users\franc\Documents\Personal_Projects\raylib_audio_visualizer\src\main.c \
+ C:/Users/franc/Documents/Personal_Projects/raylib_audio_visualizer/header_files/raylib.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/14.1.0/include/stdarg.h \
+ C:/msys64/mingw64/include/stdarg.h \
+ C:/msys64/mingw64/include/_mingw_stdarg.h \
+ C:/msys64/mingw64/include/vadefs.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/14.1.0/include/stdbool.h
